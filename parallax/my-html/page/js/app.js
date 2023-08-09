0cf6a9f4820ff8eba3046169c9e34a6812db18a2
Artiom Manokhin <amanokhin22@ukr.net>
2023-08-09 20:34:20 +0300

    Second commit. Add header, swiper and another two sections. all with media screens

--- parallax/my-html/page/js/app.js
+++ parallax/my-html/page/js/app.js
@@ -1,6 +1,17 @@
 const navbar = document.getElementById('navbar');
+const header = document.getElementById('container-fluid');
 const burgerIcon = document.getElementById('burgerIcon');
 const closeIcon = document.getElementById('closeIcon');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    if (scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
 
 burgerIcon.addEventListener('click', () => {
     navbar.classList.add('show');
@@ -13,3 +24,16 @@ closeIcon.addEventListener('click', () => {
     burgerIcon.classList.remove('hidden');
     closeIcon.classList.add('hidden');
 });
+
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+
